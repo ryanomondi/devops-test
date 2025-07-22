8f4b47b1eb36d0101426cac8bec917c24952d44c
rayo254254 <r1019767@student.thomasmore.be>
2025-07-22 13:35:13 +0200

    Add initial test status checks to ryan.py

---
+++ ryan.py
@@ -0,0 +1,11 @@
+if tests == running:
+    print("Test are running successfully and passed")
+else:
+    print("Test are not running")
+if __name__ == "__main__":
+    print("This is the main module of ryan.py")
+else:
+    print("This is not the main module of ryan.py")
+
+tests = "running"
+running = "running"
