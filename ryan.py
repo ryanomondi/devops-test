66ca423ab570d33991eb20cecc4f47073d7f86bc
rayo254254 <r1019767@student.thomasmore.be>
2025-07-24 12:29:54 +0200

    test

--- ryan.py
+++ ryan.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     print("This is the main module of ryan.py")
 else:
     print("This is not the main module of ryan.py")
-
+k
 tests = "running"
 running = "running"
 print("This is the ryan.py module")
